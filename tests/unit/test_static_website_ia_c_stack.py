e884d5b2c0bbf414f57b4055cf280edb46344fe9
Jeston Lewis <jeston.lewis@protonmail.com>
2023-04-26 18:37:24 -0400

    changed a couple names associated with reference files

--- tests/unit/test_static_website_ia_c_stack.py
+++ tests/unit/test_static_website_ia_c_stack.py
@@ -1,7 +1,7 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from static_website_ia_c.static_website_ia_c_stack import StaticWebsiteIaCStack
+from stack.static_website_stack import StaticWebsiteIaCStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in static_website_ia_c/static_website_ia_c_stack.py
