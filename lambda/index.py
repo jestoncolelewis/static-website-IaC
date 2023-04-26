e8fa8b0e77ea385761f49ef87d4407d3a86d094f
Jeston Lewis <jeston.lewis@protonmail.com>
2023-04-26 18:37:54 -0400

    removed excess logic

--- lambda/index.py
+++ lambda/index.py
@@ -2,16 +2,11 @@ import boto3
 
 dynamo = boto3.client('dynamodb')
 dynamor = boto3.resource('dynamodb')
-table = dynamor.Table('breadtest') # type: ignore
+table = dynamor.Table('breadtest')
 
 def handler(event, context):
     response = ''
-    if event.get('') == ...:
-        posts = 12
-    elif event.get('') == ...:
-        posts = 8
-    else:
-        posts = table.item_count
+    posts = table.item_count
 
     for i in range(posts):
         item = dynamo.get_item(
