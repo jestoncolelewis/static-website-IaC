8de98d8fba8d3a00c494fbb3980bd5d4b94f84a7
Jeston Lewis <jeston.lewis@protonmail.com>
2023-04-27 17:32:48 -0400

    pipeline boiler plate

---
+++ stack/pipeline_stack.py
@@ -0,0 +1,17 @@
+from constructs import Construct
+from aws_cdk import (
+    Stack,
+    aws_codecommit as codecommit,
+    pipelines as pipelines
+)
+from stack.pipeline_stage import StaticWebsitePipeline
+
+class StaticWebsitePipelineStack(Stack):
+
+    def __init__(self, scope: Construct, id: str, **kwargs):
+        super().__init__(scope, id, **kwargs)
+
+        repo = codecommit.Repository(
+            self, 'Repo',
+            repository_name=''
+        )
---
+++ stack/pipeline_stage.py
@@ -0,0 +1,12 @@
+from constructs import Construct
+from aws_cdk import (
+    Stage
+)
+from .static_website_stack import StaticWebsiteIaCStack
+
+class StaticWebsitePipeline(Stage):
+
+    def __init__(self, scope: Construct, id: str, **kwargs):
+        super().__init__(scope, id, **kwargs)
+
+        service = StaticWebsiteIaCStack(self, 'WebService')
