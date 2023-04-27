8dcdf84664e5263fc8e4753ab78baa8bf6896281
Jeston Lewis <jeston.lewis@protonmail.com>
2023-04-27 11:59:06 -0400

    added hosting zone records

--- stack/static_website_stack.py
+++ stack/static_website_stack.py
@@ -5,7 +5,10 @@ from aws_cdk import (
     aws_apigateway as apigw,
     aws_dynamodb as ddb,
     aws_ses as ses,
-    aws_route53 as r53
+    aws_route53 as r53,
+    aws_route53_targets as targets,
+    aws_cloudfront as cf,
+    aws_cloudfront_origins as origins
 )
 from constructs import Construct
 
@@ -16,6 +19,7 @@ class StaticWebsiteIaCStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs):
         super().__init__(scope, construct_id, **kwargs)
 
+        # Lambda
         func = alamb.Function(
             self, 'FunctionHandler',
             runtime=alamb.Runtime.PYTHON_3_9,
@@ -23,6 +27,7 @@ class StaticWebsiteIaCStack(Stack):
             handler='index.handler'
         )
 
+        # Buckets
         main_bucket = s3.Bucket(
             self, 'PublicBucket',
             public_read_access=True,
@@ -37,21 +42,41 @@ class StaticWebsiteIaCStack(Stack):
             bucket_name='www.' + name
         )
 
+        # API
         gateway = apigw.LambdaRestApi(
             self, 'Endpoint',
             handler=func
         )
 
+        # Dynamo
         table = ddb.Table(
             self, name + 'Table',
             partition_key={'name': 'path', 'type': ddb.AttributeType.STRING}
         )
 
+        # Hosting
         zone = r53.HostedZone(
             self, 'HostedZone',
             zone_name=name
         )
 
+        main_record = r53.RecordSet(
+            self, 'MainRecord',
+            record_type=r53.RecordType.A,
+            target=r53.RecordTarget.from_alias(targets.BucketWebsiteTarget(main_bucket)),
+            zone=zone,
+            record_name=name
+        )
+
+        www_record = r53.RecordSet(
+            self, 'wwwRecord',
+            record_type=r53.RecordType.A,
+            target=r53.RecordTarget.from_alias(targets.BucketWebsiteTarget(www_bucket)),
+            zone=zone,
+            record_name='www.' + name
+        )
+
+        # SES
         identity = ses.EmailIdentity(
             self, 'Identity',
             identity=ses.Identity.public_hosted_zone(zone),
