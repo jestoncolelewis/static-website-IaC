3f528292c4eddfbfef83c9c1d3ecf5477f75aa5b
Jeston Lewis <jeston.lewis@protonmail.com>
2023-04-27 11:59:46 -0400

    added environment

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import aws_cdk as cdk
 
 from stack.static_website_stack import StaticWebsiteIaCStack
 
-
+env_USA = cdk.Environment(account='706391136734', region='us-west-2')
 app = cdk.App()
-StaticWebsiteIaCStack(app, "StaticWebsiteIaCStack")
+StaticWebsiteIaCStack(app, "StaticWebsiteIaCStack", env=env_USA)
 
 app.synth()
