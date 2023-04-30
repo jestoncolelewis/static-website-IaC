f92c50033bf548ca5cb73f9b2ad0f48dd59be0ca
Jeston Lewis <jeston.lewis@protonmail.com>
2023-04-29 23:11:41 -0600

    updated upload variable creation

--- stack/upload.py
+++ stack/upload.py
@@ -15,14 +15,12 @@ key_name = "" # name for primary key
 # s3 web variables
 to_upload = {}
 
-for path in os.walk('..'):
+for path in os.walk('../website'):
     with os.scandir(path[0]) as it:
         files = []
         for entry in it:
-            if not entry.name.startswith('.') and not entry.name.endswith('.md') and not entry.name.endswith('.http') and not entry.name.endswith('.py') and not entry.name.endswith('ipynb') and not path[0].startswith('../.') and entry.is_file():
-                files.append(entry.name)
-        if not path[0].startswith('../.') and not path[0].startswith('../IaC') and len(files) != 0:
-            to_upload[path[0]] = files
+            files.append(entry.name)
+        to_upload[path[0]] = files
 
 # build s3 for website
 def build_web_bucket(name, objects):
