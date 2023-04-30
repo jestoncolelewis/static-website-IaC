5c0aba404664721500020294f70a9ea5d10b207d
Jeston Lewis <jeston.lewis@protonmail.com>
2023-04-30 11:38:29 -0600

    finalizing ability to update and upload files

--- app.py
+++ app.py
@@ -3,10 +3,16 @@ import os
 
 import aws_cdk as cdk
 
-from stack.pipeline_stack import StaticWebsitePipelineStack
+from stack.pipeline_stack import StaticWebsitePipelineStack, name
+from stack.static_website_stack import StaticWebsiteIaCStack
+from stack.upload import upload, update
 
 env_USA = cdk.Environment(account='706391136734', region='us-west-2')
 app = cdk.App()
 StaticWebsitePipelineStack(app, "StaticWebsiteIaCStack", env=env_USA)
 
 app.synth()
+
+update(endpoint = StaticWebsiteIaCStack.)
+
+upload(name)
