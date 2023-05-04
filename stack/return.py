36fb00650f1a29859a236c2c7b7191b53d429567
Jeston Lewis <jeston.lewis@protonmail.com>
2023-05-04 11:51:16 -0700

    defined properties and included the necessary cdk

--- stack/return.py
+++ stack/return.py
@@ -5,7 +5,27 @@ from aws_cdk import (
 )
 
 class PostReturn(Construct):
+    @property
+    def handler(self):
+        return self._handler
+    
+    @property
+    def table(self):
+        return self._table
+    
     def __init__(self, scope:Construct, id: str, **kwargs):
         super().__init__(scope, id, **kwargs)
 
-        # TODO
+        # Post Lambda
+        self._handler = alamb.Function(
+            self, 'PostFunctionHandler',
+            runtime=alamb.Runtime.PYTHON_3_9,
+            code=alamb.Code.from_asset('lambda'),
+            handler='post_return.handler'
+        )
+
+        # Dynamo
+        self._table = ddb.Table(
+            self, 'Table',
+            partition_key={'name': 'path', 'type': ddb.AttributeType.STRING}
+        )
