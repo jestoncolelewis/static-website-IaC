cfc9192c41bb49da9254640e0105d9cb4a7bd4dc
Jeston Lewis <jeston.lewis@protonmail.com>
2023-05-04 12:03:06 -0700

    defined resources for the new construct

--- stack/form.py
+++ stack/form.py
@@ -5,7 +5,28 @@ from aws_cdk import (
 )
 
 class FormSubmit(Construct):
+    @property
+    def handler(self):
+        return self._handler
+    
+    @property
+    def identity(self):
+        return self._identity
+
     def __init__(self, scope:Construct, id: str, **kwargs):
         super().__init__(scope, id, **kwargs)
 
-        # TODO
+        # Form Lambda
+        self._handler = alamb.Function(
+            self, 'FormFunctionHandler',
+            runtime=alamb.Runtime.PYTHON_3_9,
+            code=alamb.Code.from_asset('lambda'),
+            handler='form_submit.handler'
+        )
+
+        # SES
+        self._identity = ses.EmailIdentity(
+            self, 'Identity',
+            identity=ses.Identity.public_hosted_zone(zone),
+            mail_from_domain=name
+        )
