e0d10ec95384d22981540820481a960b77b735af
Jeston Lewis <jeston.lewis@protonmail.com>
2023-05-05 14:45:17 -0600

    cleaned up property location

--- stack/hosting.py
+++ stack/hosting.py
@@ -3,28 +3,29 @@ from aws_cdk import (
     aws_route53 as r53,
     aws_route53_targets as targets
 )
-from .static_website_stack import StaticWebsiteIaCStack, name
+from .buckets import Buckets, name
 
 class Hosting(Construct):
-    @property
-    def zone(self):
-        return self._zone
-
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
+
+        @property
+        def zone(self):
+            return self._zone
+
         # Hosting
-        zone = r53.HostedZone(
+        hosted_zone = r53.HostedZone(
             self, 'HostedZone',
             zone_name=name
         )
 
-        bucket = StaticWebsiteIaCStack(self, 'Buckets')
+        bucket = Buckets(self, 'Buckets')
 
         main_record = r53.RecordSet(
             self, 'MainRecord',
             record_type=r53.RecordType.A,
             target=r53.RecordTarget.from_alias(targets.BucketWebsiteTarget(bucket._main_bucket)),
-            zone=zone,
+            zone=hosted_zone,
             record_name=name
         )
 
@@ -32,8 +33,8 @@ class Hosting(Construct):
             self, 'wwwRecord',
             record_type=r53.RecordType.A,
             target=r53.RecordTarget.from_alias(targets.BucketWebsiteTarget(bucket._www_bucket)),
-            zone=zone,
+            zone=hosted_zone,
             record_name='www.' + name
         )
 
-        self._zone = zone
+        self._zone = hosted_zone
