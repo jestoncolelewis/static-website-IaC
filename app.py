e6476b42a3caf607f4a08270f08dafedc281d263
Jeston Lewis <jeston.lewis@protonmail.com>
2023-05-05 14:44:31 -0600

    removed unused import

--- app.py
+++ app.py
@@ -2,9 +2,9 @@
 import aws_cdk as cdk
 
 from stack.pipeline_stack import StaticWebsitePipelineStack
-from stack.upload import upload, update
 
 env_USA = cdk.Environment(account='706391136734', region='us-west-2')
+
 app = cdk.App()
 StaticWebsitePipelineStack(app, "StaticWebsitePipelineStack", env=env_USA)
 
