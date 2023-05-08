1e31dad2168f085d36446059321a4c5abc0547c3
Jeston Lewis <jeston.lewis@protonmail.com>
2023-05-08 15:23:16 -0600

    start of pipeline build

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import aws_cdk as cdk
 
-from stack.static_website_stack import StaticWebsiteIaCStack
+from stack.pipeline_stack import StaticWebsitePipelineStack
 
 app = cdk.App()
-StaticWebsiteIaCStack(app, "StaticWebsiteIaCStack", env=cdk.Environment(account='706391136734', region='us-west-2'))
+StaticWebsitePipelineStack(app, "StaticWebsiteIaCStack", env=cdk.Environment(account='706391136734', region='us-west-2'))
 
 app.synth()
--- stack/pipeline_stack.py
+++ stack/pipeline_stack.py
@@ -1,10 +1,6 @@
 from constructs import Construct
-from aws_cdk import (
-    Stack,
-    aws_codecommit as codecommit,
-    pipelines as pipelines
-)
-from stack.pipeline_stage import StaticWebsitePipeline
+from aws_cdk import Stack
+from aws_cdk.pipelines import CodePipeline, CodePipelineSource, ShellStep
 
 name = 'mybreadventure.blog'
 
@@ -13,45 +9,16 @@ class StaticWebsitePipelineStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs):
         super().__init__(scope, id, **kwargs)
 
-        repo = codecommit.Repository(
-            self, 'Repo',
-            repository_name=name +'-Repo'
-        )
-
-        pipeline = pipelines.CodePipeline(
+        pipeline = CodePipeline(
             self, 'Pipeline',
-            synth=pipelines.ShellStep(
+            pipeline_name='StaticWebsitePipeline',
+            synth=ShellStep(
                 'Synth',
-                input=pipelines.CodePipelineSource.code_commit(repo, 'main'),
+                input=CodePipelineSource.git_hub('TODO', 'main'),
                 commands=[
-                    'npm -g install aws-cdk',
-                    'pip install -r requirements.txt',
+                    'npm instlla -g aws_cdk',
+                    'python pip install -r requirements.txt',
                     'cdk synth'
-                ]
-            )
-        )
-
-        deploy = StaticWebsitePipeline(self, 'Deploy')
-        deploy_stage = pipeline.add_stage(deploy)
-        deploy_stage.add_post(
-            pipelines.ShellStep(
-                'TestViewerEndpoint',
-                env_from_cfn_outputs={
-                    'ENDPOINT_URL': deploy._hc_viewer_url
-                },
-                commands=['curl -Ssf $ENDPOINT_URL']
-            )
-        )
-        deploy_stage.add_post(
-            pipelines.ShellStep(
-                'TestAPIGatewayEndpoint',
-                env_from_cfn_outputs={
-                    'ENDPOINT_URL': deploy._hc_endpoint
-                },
-                commands=[
-                    'curl -Ssf $ENDPOINT_URL',
-                    'curl -Ssf $ENDPOINT_URL/hello',
-                    'curl -Ssf $ENDPOINT_URL/test'
                 ]
             )
         )
