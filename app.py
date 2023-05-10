c5f2ab8ac71e219b4ab9d1df3764465e83930830
Jeston Lewis <jeston.lewis@protonmail.com>
2023-05-09 22:25:21 -0600

    added env

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ import aws_cdk as cdk
 from stack.pipeline_stack import StaticWebsitePipelineStack
 
 app = cdk.App()
-StaticWebsitePipelineStack(app, "StaticWebsiteIaCStack")
+StaticWebsitePipelineStack(app, "StaticWebsiteIaCStack", env=cdk.Environment(account='706391136734', region='us-west-2'))
 
 app.synth()
