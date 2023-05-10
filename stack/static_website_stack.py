f1b6da022789c0456561f8877039dc5e46cb7402
Jeston Lewis <jeston.lewis@protonmail.com>
2023-05-10 12:01:03 -0600

    defined attributes

--- stack/static_website_stack.py
+++ stack/static_website_stack.py
@@ -11,13 +11,21 @@ class StaticWebsiteIaCStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs):
         super().__init__(scope, construct_id, **kwargs)
 
+        @property
+        def post_endpoint(self):
+            return self._post_endpoint
+        
+        @property
+        def form_endpoint(self):
+            return self._form_endpoint
+
         # API
         post = PostReturn(self, 'PostReturn')
         post_gateway = apigw.LambdaRestApi(self, 'PostEndpoint', handler=post._handler)# type: ignore
         
-        self.post_endpoint = CfnOutput(self, 'PostGatewayUrl', value=post_gateway.url)
+        self._post_endpoint = CfnOutput(self, 'PostGatewayUrl', value=post_gateway.url)
 
         form = FormSubmit(self, 'FormSubmit')
         form_gateway = apigw.LambdaRestApi(self, 'FormEndpoint', handler=form._handler)# type: ignore
 
-        self.form_endpoint = CfnOutput(self, 'FormGatewayUrl', value=form_gateway.url)       
+        self._form_endpoint = CfnOutput(self, 'FormGatewayUrl', value=form_gateway.url)
